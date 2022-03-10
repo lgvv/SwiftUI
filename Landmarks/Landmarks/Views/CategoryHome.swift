38eb57797fcddb5d8c4d2dbc71d099bb43f6bb41
lgvv <lgw9898@naver.com>
2022-03-10 15:50:25 +0900

    [ADD] Section 1 Add a Category View

---
+++ Landmarks/Landmarks/Views/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Hamlit Jason on 2022/03/10.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+        
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
