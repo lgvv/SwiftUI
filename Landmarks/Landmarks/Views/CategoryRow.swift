ffc8ad723aed8d98cd408d333614eed5fc2dffef
lgvv <lgw9898@naver.com>
2022-03-10 16:23:46 +0900

    [ADD] Section 3 Create a Category Row

---
+++ Landmarks/Landmarks/Views/CategoryRow.swift
@@ -0,0 +1,41 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Hamlit Jason on 2022/03/10.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        CategoryItem(landmark: landmark)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var lanmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(
+            categoryName: lanmarks[0].category.rawValue,
+            items: Array(lanmarks.prefix(4))
+        )
+    }
+}
