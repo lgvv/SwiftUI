2f28c7a5edaca77c73c429327baca401c963842f
lgvv <lgw9898@naver.com>
2022-03-14 21:12:14 +0900

    [ADD] Section 7 Add Preferences with a Settings Scene

--- Landmarks/Landmarks/Views/Helpers/MapView.swift
+++ Landmarks/Landmarks/Views/Helpers/MapView.swift
@@ -10,20 +10,36 @@ import MapKit
 
 struct MapView: View {
     var coordinate: CLLocationCoordinate2D
-    @State private var region = MKCoordinateRegion()
-    
-    var body: some View {
-        // $란 prefix는 참조에 대한 바인딩을 제공
-        Map(coordinateRegion: $region)
-            .onAppear {
-                setRegion(coordinate)
-            }
+
+    @AppStorage("MapView.zoom")
+    private var zoom: Zoom = .medium
+
+    enum Zoom: String, CaseIterable, Identifiable {
+        case near = "Near"
+        case medium = "Medium"
+        case far = "Far"
+
+        var id: Zoom {
+            return self
+        }
     }
-    
-    private func setRegion(_ coordinate: CLLocationCoordinate2D) {
-        region = MKCoordinateRegion(
+
+    var delta: CLLocationDegrees {
+        switch zoom {
+        case .near: return 0.02
+        case .medium: return 0.2
+        case .far: return 2
+        }
+    }
+
+    var body: some View {
+        Map(coordinateRegion: .constant(region))
+    }
+
+    var region: MKCoordinateRegion {
+        MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
         )
     }
 }
@@ -31,6 +47,5 @@ struct MapView: View {
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
-        
     }
 }
